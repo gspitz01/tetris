9121214f215341a4c27162b2726a8effe8780d12
Greg Spitz <gregspitz6@gmail.com>
2018-05-18 13:19:48 -0400

    Reorganize Game for testing
    Change createGuiAndRun to use injection

--- src/main/java/com/gregspitz/tetris/Game.java
+++ src/main/java/com/gregspitz/tetris/Game.java
@@ -24,21 +24,21 @@ public class Game {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(Game::createGuiAndRun);
+        SwingUtilities.invokeLater(() -> {
+            GameModel model = new GameModel();
+            GameView view = new GameView(model);
+            GameKeyListener keyListener = new GameKeyListener(model, view);
+            view.addKeyListener(keyListener);
+            GameTimer timer = new GameTimer(model, view);
+            Game game = new Game(model, view, timer);
+            createGuiAndRun(new JFrame("Tetris"), view, game);
+        });
     }
 
-    // View
-    private static void createGuiAndRun() {
-        JFrame gameFrame = new JFrame("Tetris");
+    static void createGuiAndRun(JFrame gameFrame, GameView view, Game game) {
         Dimension preferredSize = new Dimension(FRAME_WIDTH, FRAME_HEIGHT);
         gameFrame.setPreferredSize(preferredSize);
         gameFrame.getContentPane().setLayout(new BorderLayout());
-        GameModel model = new GameModel();
-        GameView view = new GameView(model);
-        GameKeyListener keyListener = new GameKeyListener(model, view);
-        view.addKeyListener(keyListener);
-        GameTimer timer = new GameTimer(model, view);
-        Game game = new Game(model, view, timer);
         gameFrame.add(view, BorderLayout.CENTER);
         gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         gameFrame.pack();
--- src/test/java/com/gregspitz/tetris/GameTest.java
+++ src/test/java/com/gregspitz/tetris/GameTest.java
@@ -5,7 +5,13 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import javax.swing.*;
+
+import java.awt.*;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for {@link Game}
@@ -22,11 +28,21 @@ public class GameTest {
     @Mock
     private GameTimer timer;
 
+    @Mock
+    private JFrame frame;
+
+    @Mock
+    private Container contentPane;
+
+    @Mock
+    private Game mockGame;
+
     private Game game;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        when(frame.getContentPane()).thenReturn(contentPane);
         game = new Game(model, view, timer);
     }
 
@@ -34,5 +50,18 @@ public class GameTest {
     public void start_callsStartOnTimer() {
         game.start();
         verify(timer).start();
+    }
+
+    @Test
+    public void createGuiAndRun_setsUpGameFrame() {
+        Game.createGuiAndRun(frame, view, mockGame);
+        verify(frame).setPreferredSize(any(Dimension.class));
+        verify(frame).getContentPane();
+        verify(contentPane).setLayout(any(BorderLayout.class));
+        verify(frame).add(view, BorderLayout.CENTER);
+        verify(frame).setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        verify(frame).pack();
+        verify(frame).setVisible(true);
+        verify(mockGame).start();
     }
 }
