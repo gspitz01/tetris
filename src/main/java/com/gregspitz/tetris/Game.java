ace6ed2f7331633e31418ace4e4631bfd81979b8
Greg Spitz <gregspitz6@gmail.com>
2018-05-18 13:33:50 -0400

    Reorganize Game

--- src/main/java/com/gregspitz/tetris/Game.java
+++ src/main/java/com/gregspitz/tetris/Game.java
@@ -5,23 +5,8 @@ import java.awt.*;
 
 public class Game {
 
-    // TODO: separate some of this into other classes based on comments
     private static final int FRAME_WIDTH = 1000;
     private static final int FRAME_HEIGHT = 1200;
-
-    private GameTimer timer;
-    private GameModel model;
-    private GameView view;
-
-    Game(GameModel model, GameView view, GameTimer timer) {
-        this.model = model;
-        this.view = view;
-        this.timer = timer;
-    }
-
-    void start() {
-        timer.start();
-    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
@@ -30,19 +15,20 @@ public class Game {
             GameKeyListener keyListener = new GameKeyListener(model, view);
             view.addKeyListener(keyListener);
             GameTimer timer = new GameTimer(model, view);
-            Game game = new Game(model, view, timer);
-            createGuiAndRun(new JFrame("Tetris"), view, game);
+            Dimension preferredSize = new Dimension(FRAME_WIDTH, FRAME_HEIGHT);
+            LayoutManager layout = new BorderLayout();
+            createGuiAndRun(new JFrame("Tetris"), preferredSize, layout, view, timer);
         });
     }
 
-    static void createGuiAndRun(JFrame gameFrame, GameView view, Game game) {
-        Dimension preferredSize = new Dimension(FRAME_WIDTH, FRAME_HEIGHT);
+    static void createGuiAndRun(JFrame gameFrame, Dimension preferredSize, LayoutManager layout,
+                                GameView view, GameTimer timer) {
         gameFrame.setPreferredSize(preferredSize);
-        gameFrame.getContentPane().setLayout(new BorderLayout());
+        gameFrame.getContentPane().setLayout(layout);
         gameFrame.add(view, BorderLayout.CENTER);
         gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         gameFrame.pack();
         gameFrame.setVisible(true);
-        game.start();
+        timer.start();
     }
 }
--- src/test/java/com/gregspitz/tetris/GameTest.java
+++ src/test/java/com/gregspitz/tetris/GameTest.java
@@ -6,10 +6,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import javax.swing.*;
-
 import java.awt.*;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -17,10 +15,6 @@ import static org.mockito.Mockito.when;
  * Tests for {@link Game}
  */
 public class GameTest {
-
-
-    @Mock
-    private GameModel model;
 
     @Mock
     private GameView view;
@@ -35,33 +29,27 @@ public class GameTest {
     private Container contentPane;
 
     @Mock
-    private Game mockGame;
+    private Dimension preferredSize;
 
-    private Game game;
+    @Mock
+    private LayoutManager layout;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
         when(frame.getContentPane()).thenReturn(contentPane);
-        game = new Game(model, view, timer);
-    }
-
-    @Test
-    public void start_callsStartOnTimer() {
-        game.start();
-        verify(timer).start();
     }
 
     @Test
     public void createGuiAndRun_setsUpGameFrame() {
-        Game.createGuiAndRun(frame, view, mockGame);
-        verify(frame).setPreferredSize(any(Dimension.class));
+        Game.createGuiAndRun(frame, preferredSize, layout, view, timer);
+        verify(frame).setPreferredSize(preferredSize);
         verify(frame).getContentPane();
-        verify(contentPane).setLayout(any(BorderLayout.class));
+        verify(contentPane).setLayout(layout);
         verify(frame).add(view, BorderLayout.CENTER);
         verify(frame).setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         verify(frame).pack();
         verify(frame).setVisible(true);
-        verify(mockGame).start();
+        verify(timer).start();
     }
 }
