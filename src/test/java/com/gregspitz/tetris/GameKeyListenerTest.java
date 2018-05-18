638937c1255cca98ffc1a18f875977a4181fd902
Greg Spitz <gregspitz6@gmail.com>
2018-05-18 12:58:51 -0400

    Add tests to GameKeyListener for no interactions

--- src/test/java/com/gregspitz/tetris/GameKeyListenerTest.java
+++ src/test/java/com/gregspitz/tetris/GameKeyListenerTest.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 import java.awt.event.KeyEvent;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 /**
@@ -55,5 +56,24 @@ public class GameKeyListenerTest {
         gameKeyListener.keyPressed(keyEvent);
         verify(gameModel).moveShapeRight();
         verify(gameView).repaint();
+    }
+
+    @Test
+    public void keyPressedOther_noCallsToModelOrView() {
+        when(keyEvent.getKeyCode()).thenReturn(KeyEvent.VK_L);
+        gameKeyListener.keyPressed(keyEvent);
+        verifyNoMoreInteractions(gameModel, gameView);
+    }
+
+    @Test
+    public void keyTyped_noInteractions() {
+        gameKeyListener.keyTyped(keyEvent);
+        verifyNoMoreInteractions(gameModel, gameView, keyEvent);
+    }
+
+    @Test
+    public void keyReleased_noInteractions() {
+        gameKeyListener.keyReleased(keyEvent);
+        verifyNoMoreInteractions(gameModel, gameView, keyEvent);
     }
 }
