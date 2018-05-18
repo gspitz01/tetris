a535ff94fa4d1a968205179cc44edb1b2fae48f6
Greg Spitz <gregspitz6@gmail.com>
2018-05-17 21:11:51 -0400

    Add RightL and tests

---
+++ src/main/java/com/gregspitz/tetris/shape/RightL.java
@@ -0,0 +1,73 @@
+package com.gregspitz.tetris.shape;
+
+import java.awt.*;
+
+public class RightL extends Shape {
+
+    private int direction;
+
+    /**
+     * Right L's are always light blue
+     * Initial block positions:
+     * |1|   |1|2|3| |1|2|      |3|
+     * |2|   |4|       |3|  |1|2|4|
+     * |3|4|           |4|
+     * @param startX starting x position for 1 block
+     * @param startY starting y position for 1 block
+     */
+    public RightL(int startX, int startY) {
+        super(new Color(0.8f, 0.8f, 1.0f));
+        int[] xs = new int[NUM_BLOCKS];
+        int[] ys = new int[NUM_BLOCKS];
+        xs[0] = startX;
+        ys[0] = startY;
+        xs[1] = startX;
+        ys[1] = startY + 1;
+        xs[2] = startX;
+        ys[2] = startY + 2;
+        xs[3] = startX + 1;
+        ys[3] = startY + 2;
+        setBlocks(xs, ys);
+        direction = 0;
+    }
+
+    @Override
+    public void rotate() {
+        switch (direction) {
+            case 0:
+                getBlocks()[1].setX(getBlocks()[1].getX() + 1);
+                getBlocks()[1].setY(getBlocks()[1].getY() - 1);
+                getBlocks()[2].setX(getBlocks()[2].getX() + 2);
+                getBlocks()[2].setY(getBlocks()[2].getY() - 2);
+                getBlocks()[3].setX(getBlocks()[3].getX() - 1);
+                getBlocks()[3].setY(getBlocks()[3].getY() - 1);
+                direction = 1;
+                break;
+            case 1:
+                getBlocks()[2].setX(getBlocks()[2].getX() - 1);
+                getBlocks()[2].setY(getBlocks()[2].getY() + 1);
+                getBlocks()[3].setX(getBlocks()[3].getX() + 1);
+                getBlocks()[3].setY(getBlocks()[3].getY() + 1);
+                direction = 2;
+                break;
+            case 2:
+                getBlocks()[0].setY(getBlocks()[0].getY() + 1);
+                getBlocks()[1].setY(getBlocks()[1].getY() + 1);
+                getBlocks()[2].setX(getBlocks()[2].getX() + 1);
+                getBlocks()[2].setY(getBlocks()[2].getY() - 1);
+                getBlocks()[3].setX(getBlocks()[3].getX() + 1);
+                getBlocks()[3].setY(getBlocks()[3].getY() - 1);
+                direction = 3;
+                break;
+            case 3:
+                getBlocks()[0].setY(getBlocks()[0].getY() - 1);
+                getBlocks()[1].setX(getBlocks()[1].getX() - 1);
+                getBlocks()[2].setX(getBlocks()[2].getX() - 2);
+                getBlocks()[2].setY(getBlocks()[2].getY() + 2);
+                getBlocks()[3].setX(getBlocks()[3].getX() - 1);
+                getBlocks()[3].setY(getBlocks()[3].getY() + 1);
+                direction = 0;
+                break;
+        }
+    }
+}
---
+++ src/test/java/com/gregspitz/tetris/shape/RightLTest.java
@@ -0,0 +1,128 @@
+package com.gregspitz.tetris.shape;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for {@link RightL}
+ */
+public class RightLTest {
+
+    private static final int START_X = 0;
+    private static final int START_Y = 0;
+
+    private RightL rightL;
+
+    @Before
+    public void setup() {
+        rightL = new RightL(START_X, START_Y);
+    }
+
+    @Test
+    public void constructor_setsBlocksInRightLShape() {
+        assertInitialPosition();
+    }
+
+    @Test
+    public void moveRight_movesBlocksOneRight() {
+        rightL.moveRight();
+        assertEquals(START_X + 1, rightL.getBlocks()[0].getX());
+        assertEquals(START_Y, rightL.getBlocks()[0].getY());
+        assertEquals(START_X + 1, rightL.getBlocks()[1].getX());
+        assertEquals(START_Y + 1, rightL.getBlocks()[1].getY());
+        assertEquals(START_X + 1, rightL.getBlocks()[2].getX());
+        assertEquals(START_Y + 2, rightL.getBlocks()[2].getY());
+        assertEquals(START_X + 2, rightL.getBlocks()[3].getX());
+        assertEquals(START_Y + 2, rightL.getBlocks()[3].getY());
+    }
+
+    @Test
+    public void moveLeft_movesBlocksOneLeft() {
+        rightL.moveLeft();
+        assertEquals(START_X - 1, rightL.getBlocks()[0].getX());
+        assertEquals(START_Y, rightL.getBlocks()[0].getY());
+        assertEquals(START_X - 1, rightL.getBlocks()[1].getX());
+        assertEquals(START_Y + 1, rightL.getBlocks()[1].getY());
+        assertEquals(START_X - 1, rightL.getBlocks()[2].getX());
+        assertEquals(START_Y + 2, rightL.getBlocks()[2].getY());
+        assertEquals(START_X , rightL.getBlocks()[3].getX());
+        assertEquals(START_Y + 2, rightL.getBlocks()[3].getY());
+    }
+
+    @Test
+    public void moveDown_movesBlocksOnePositionPositiveY() {
+        rightL.moveDown();
+        assertEquals(START_X, rightL.getBlocks()[0].getX());
+        assertEquals(START_Y + 1, rightL.getBlocks()[0].getY());
+        assertEquals(START_X, rightL.getBlocks()[1].getX());
+        assertEquals(START_Y + 2, rightL.getBlocks()[1].getY());
+        assertEquals(START_X, rightL.getBlocks()[2].getX());
+        assertEquals(START_Y + 3, rightL.getBlocks()[2].getY());
+        assertEquals(START_X + 1, rightL.getBlocks()[3].getX());
+        assertEquals(START_Y + 3, rightL.getBlocks()[3].getY());
+    }
+
+    @Test
+    public void rotateOnce_turnsShapeLDown() {
+        rightL.rotate();
+        assertEquals(START_X, rightL.getBlocks()[0].getX());
+        assertEquals(START_Y, rightL.getBlocks()[0].getY());
+        assertEquals(START_X + 1, rightL.getBlocks()[1].getX());
+        assertEquals(START_Y, rightL.getBlocks()[1].getY());
+        assertEquals(START_X + 2, rightL.getBlocks()[2].getX());
+        assertEquals(START_Y, rightL.getBlocks()[2].getY());
+        assertEquals(START_X, rightL.getBlocks()[3].getX());
+        assertEquals(START_Y + 1, rightL.getBlocks()[3].getY());
+    }
+
+    @Test
+    public void rotateTwice_turnsShapeLLeft() {
+        rightL.rotate();
+        rightL.rotate();
+        assertEquals(START_X, rightL.getBlocks()[0].getX());
+        assertEquals(START_Y, rightL.getBlocks()[0].getY());
+        assertEquals(START_X + 1, rightL.getBlocks()[1].getX());
+        assertEquals(START_Y, rightL.getBlocks()[1].getY());
+        assertEquals(START_X + 1, rightL.getBlocks()[2].getX());
+        assertEquals(START_Y + 1, rightL.getBlocks()[2].getY());
+        assertEquals(START_X + 1, rightL.getBlocks()[3].getX());
+        assertEquals(START_Y + 2, rightL.getBlocks()[3].getY());
+    }
+
+    @Test
+    public void rotateThrice_turnsShapeLUp() {
+        rightL.rotate();
+        rightL.rotate();
+        rightL.rotate();
+        assertEquals(START_X, rightL.getBlocks()[0].getX());
+        assertEquals(START_Y + 1, rightL.getBlocks()[0].getY());
+        assertEquals(START_X + 1, rightL.getBlocks()[1].getX());
+        assertEquals(START_Y + 1, rightL.getBlocks()[1].getY());
+        assertEquals(START_X + 2, rightL.getBlocks()[2].getX());
+        assertEquals(START_Y, rightL.getBlocks()[2].getY());
+        assertEquals(START_X + 2, rightL.getBlocks()[3].getX());
+        assertEquals(START_Y + 1, rightL.getBlocks()[3].getY());
+    }
+
+    @Test
+    public void rotateFourTimes_returnsShapeToInitialPosition() {
+        rightL.rotate();
+        rightL.rotate();
+        rightL.rotate();
+        rightL.rotate();
+        assertInitialPosition();
+    }
+
+    private void assertInitialPosition() {
+        assertEquals(START_X, rightL.getBlocks()[0].getX());
+        assertEquals(START_Y, rightL.getBlocks()[0].getY());
+        assertEquals(START_X, rightL.getBlocks()[1].getX());
+        assertEquals(START_Y + 1, rightL.getBlocks()[1].getY());
+        assertEquals(START_X, rightL.getBlocks()[2].getX());
+        assertEquals(START_Y + 2, rightL.getBlocks()[2].getY());
+        assertEquals(START_X + 1, rightL.getBlocks()[3].getX());
+        assertEquals(START_Y + 2, rightL.getBlocks()[3].getY());
+    }
+}
